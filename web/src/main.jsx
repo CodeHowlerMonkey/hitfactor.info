34ab0fd23baadc513888623a5b2b2a6e0c9abaff
Howler Monkey <howlermonkey@uspsa.org>
2024-02-12 09:17:30 -0500

    web: refactor main.jsx to name bigger components pages

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,125 +1,140 @@
-import React, {useState, Suspense, useEffect} from 'react'
-import ReactDOM from 'react-dom/client'
-import {PrimeReactProvider} from 'primereact/api'
-import {TabMenu} from 'primereact/tabmenu'
-import 'primereact/resources/themes/soho-dark/theme.css'
-import 'primeicons/primeicons.css'
-import 'primeflex/primeflex.css'
+import React, { useState, Suspense, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { PrimeReactProvider } from "primereact/api";
+import { TabMenu } from "primereact/tabmenu";
+import "primereact/resources/themes/soho-dark/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 import {
   createBrowserRouter,
   Outlet,
   RouterProvider,
   useNavigate,
   useLocation,
-} from 'react-router-dom'
-import './index.css'
+} from "react-router-dom";
+import "./index.css";
 
-export const App = React.lazy(() => import('./App.jsx'))
-export const ClassificationStats = React.lazy(() => import('./classifications'))
-export const Classifiers = React.lazy(() => import('./classifiers'))
+export const AppPage = React.lazy(() => import("./App.jsx"));
+export const ClassificationStatsPage = React.lazy(() =>
+  import("./classifications")
+);
+export const ClassifiersPage = React.lazy(() => import("./classifiers"));
 
 const UnderConstruction = () => (
   <>
     <h2>I'm Digging As Fast As I Can</h2>
-    <h3 style={{fontStyle: 'italic'}}>503 Under Construction</h3>
+    <h3 style={{ fontStyle: "italic" }}>503 Under Construction</h3>
   </>
-)
-const Clubs = UnderConstruction
-const Shooters = UnderConstruction
+);
+const ClubsPage = UnderConstruction;
+const ShootersPage = UnderConstruction;
+const UploadPage = UnderConstruction;
 
 const config = [
   {
-    label: 'Classification Stats',
-    icon: 'pi pi-chart-pie',
-    path: '/classifications',
+    label: "Classification Stats",
+    icon: "pi pi-chart-pie",
+    path: "/classifications",
   },
   {
-    label: 'Classifiers',
-    icon: 'pi pi-chart-bar',
-    path: '/classifiers',
+    label: "Classifiers",
+    icon: "pi pi-chart-bar",
+    path: "/classifiers",
   },
   {
-    label: 'Shooters',
-    icon: 'pi pi-users',
-    path: '/shooters',
+    label: "Shooters",
+    icon: "pi pi-users",
+    path: "/shooters",
   },
   {
-    label: 'Clubs',
-    icon: 'pi pi-sitemap',
-    path: '/clubs',
+    label: "Clubs",
+    icon: "pi pi-sitemap",
+    path: "/clubs",
   },
   {
-    label: 'Bad Link',
-    icon: 'pi pi-ban',
-    path: '/bad',
+    label: "Upload",
+    icon: "pi pi-upload",
+    path: "/upload",
   },
-]
+  /*
+  {
+    label: "Bad Link",
+    icon: "pi pi-ban",
+    path: "/bad",
+  },*/
+];
 const Menu = () => {
-  const navigate = useNavigate()
-  const {pathname} = useLocation()
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
   const [activeIndex, setActiveIndex] = useState(
-    config.map(c => c.path).findIndex(curPath => pathname.startsWith(curPath)),
-  )
+    config
+      .map((c) => c.path)
+      .findIndex((curPath) => pathname.startsWith(curPath))
+  );
 
   return (
     <TabMenu
-      style={{fontSize: '1.25rem'}}
-      model={config.map(c => ({...c, command: () => navigate(c.path)}))}
+      style={{ fontSize: "1.25rem" }}
+      model={config.map((c) => ({ ...c, command: () => navigate(c.path) }))}
       activeIndex={activeIndex}
-      onTabChange={e => setActiveIndex(e.index)}
+      onTabChange={(e) => setActiveIndex(e.index)}
     />
-  )
-}
+  );
+};
 
 const Layout = () => (
   <div className="card">
     <Menu />
     <Outlet />
   </div>
-)
+);
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     Component: Layout,
     errorElement: (
       <div className="card">
         <Menu />
         <h2>Sent It A Bit Too Hard, Bud!</h2>
-        <h3 style={{fontStyle: 'italic'}}>404 Not Found</h3>
+        <h3 style={{ fontStyle: "italic" }}>404 Not Found</h3>
       </div>
     ),
     children: [
       {
-        path: 'app',
-        Component: App,
+        path: "app",
+        Component: AppPage,
       },
       {
-        path: 'classifications',
-        Component: ClassificationStats,
+        path: "classifications",
+        Component: ClassificationStatsPage,
       },
       {
-        path: 'classifiers/:division?/:classifier?',
-        Component: Classifiers,
+        path: "classifiers/:division?/:classifier?",
+        Component: ClassifiersPage,
       },
       {
-        path: 'shooters',
-        Component: Shooters,
+        path: "shooters",
+        Component: ShootersPage,
       },
       {
-        path: 'clubs',
-        Component: Clubs,
+        path: "clubs",
+        Component: ClubsPage,
+      },
+      {
+        path: "upload",
+        Component: UploadPage,
       },
     ],
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <PrimeReactProvider>
       <Suspense fallback={<div>loading</div>}>
         <RouterProvider router={router} />
       </Suspense>
     </PrimeReactProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
