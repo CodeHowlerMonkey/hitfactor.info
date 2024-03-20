276de53257dfd2f10d81e8d8a84fc8f08e04b43f
pzsette <pietrozarri96@gmail.com>
2024-03-20 01:06:35 +0100

    add club find logic

--- api/src/routes/api/clubs/index.js
+++ api/src/routes/api/clubs/index.js
@@ -1,5 +1,28 @@
-const clubsRoutes = async (fastify, opts) => {
-  fastify.get("/", (req, res) => 'Clubs!')
+import { processImport } from "../../../utils.js";
+
+const clubsRoutes = async (fastify) => {
+  fastify.get("/", () => getClubs())
+}
+export const getClubs = async () => {
+  const result = [];
+  const clubDict = {};
+
+  processImport(
+    "../../data/imported",
+    /classifiers\.\d+\.json/,
+    ({ value = {} }) => {
+      const { classifiers = [] } = value
+      for (const { division_classifiers = {} } of classifiers) {
+        for (const { clubid, club_name } of division_classifiers) {
+          if (clubid && club_name) { clubDict[clubid] = club_name }
+        }
+      }
+    }
+  );
+  Object.keys(clubDict).forEach(function(key) {
+    result.push({ id: key, name: clubDict[key] })
+  })
+  return result;
 }
 
 export default clubsRoutes;
