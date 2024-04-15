5056f35b72ab000a4a27912bd054b32c4aed56e2
Howler Monkey <howlermonkey@uspsa.org>
2024-04-15 03:55:35 -0600

    db/shooters: refactor into single shooter doc obj

--- api/src/db/shooters.js
+++ api/src/db/shooters.js
@@ -1,18 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
 import { loadJSON, processImportAsync } from "../utils.js";
-import {
-  divIdToShort,
-  mapDivisions,
-  mapDivisionsAsync,
-} from "../dataUtil/divisions.js";
-import { Percent } from "../dataUtil/numbers.js";
+import { divIdToShort, mapDivisions, mapDivisionsAsync } from "../dataUtil/divisions.js";
 import {
   calculateUSPSAClassification,
   classForPercent,
   rankForClass,
 } from "../../../shared/utils/classification.js";
-import { safeNumSort } from "../../../shared/utils/sort.js";
 
 import { Score } from "./scores.js";
 
@@ -76,44 +70,7 @@ export const scoresAge = async (division, memberNumber, maxScores = 4) => {
     .reduce((acc, curV, unusedIndex, arr) => acc + curV / arr.length, 0);
 };
 
-const DivReclassificationSchema = new Schema({
-  class: String,
-  percent: Number,
-  highPecent: Number,
-  percentWithDates: [{ p: Number, sd: Date }],
-});
-
-const OldShooterSchema = new Schema(
-  {
-    memberId: String,
-    memberNumber: String, // TODO: try unique on that and Id + division
-    name: String,
-
-    classifications: { type: Map, of: String },
-    high: { type: Map, of: Number | NaN },
-    current: { type: Map, of: Number | NaN },
-
-    reclassificationsByCurPercent: { type: Map, of: DivReclassificationSchema },
-    reclassificationsByRecPercent: { type: Map, of: DivReclassificationSchema },
-
-    ages: { type: Map, of: Number },
-
-    data: {
-      member_id: String,
-      member_number: String,
-      first_name: String,
-      middle_name: String,
-      last_name: String,
-      suffix: String,
-      salutation: String,
-      joined_date: Date,
-      expiration_date: Date,
-      life_member: Boolean,
-      privacy: Boolean,
-    },
-  },
-  { strict: false }
-);
+// TODO: write up the schema once it's stable
 const ShooterSchema = new Schema({}, { strict: false });
 ShooterSchema.index({ memberNumber: 1, division: 1 });
 ShooterSchema.index({ memberNumber: 1 });
@@ -133,11 +90,18 @@ export const reduceByDiv = (classifications, valueFn) =>
 
 // TODO: can I do lean here?
 const allDivisionsScores = async (memberNumber) => {
-  const query = Score.find({ memberNumber }).limit(0).sort({ sd: -1 });
+  const query = Score.find({ memberNumber }).populate("HHFs").limit(0).sort({ sd: -1 });
   const data = await query;
 
   return data.map((doc) => doc.toObject({ virtuals: true }));
 };
+
+const reclassificationBreakdown = (reclassificationInfo, division) => ({
+  current: reclassificationInfo?.[division]?.percent,
+  currents: mapDivisions((div) => reclassificationInfo?.[div]?.percent),
+  class: classForPercent(reclassificationInfo?.[division]?.percent),
+  classes: mapDivisions((div) => classForPercent(reclassificationInfo?.[div]?.percent)),
+});
 
 export const hydrateShooters = async () => {
   console.log("hydrating shooters");
@@ -149,147 +113,71 @@ export const hydrateShooters = async () => {
     async ({ value: c }) => {
       const memberNumber = memberNumberFromMemberData(c.member_data);
       const memberScores = await allDivisionsScores(memberNumber);
-
-      const shooterDoc = {
-        data: c.member_data,
-        memberId: c.member_data.member_id,
-        memberNumber,
-        name: [
-          c.member_data.first_name,
-          c.member_data.last_name,
-          c.member_data.suffix,
-        ]
-          .filter(Boolean)
-          .join(" "),
-        classifications: reduceByDiv(c.classifications, (r) => r.class),
-        high: reduceByDiv(c.classifications, (r) => Number(c.high_percent)),
-        current: reduceByDiv(c.classifications, (r) =>
-          Number(c.current_percent)
-        ),
-        reclassificationsByCurPercent: calculateUSPSAClassification(
-          memberScores,
-          "curPercent"
-        ),
-        reclassificationsByRecPercent: calculateUSPSAClassification(
-          memberScores,
-          "recPercent"
-        ),
-        ages: await mapDivisionsAsync((div) => scoresAge(div, memberNumber)),
-        age1s: await mapDivisionsAsync((div) =>
-          scoresAge(div, memberNumber, 1)
-        ),
-      };
+      const ages = await mapDivisionsAsync((div) => scoresAge(div, memberNumber));
+      const age1s = await mapDivisionsAsync((div) => scoresAge(div, memberNumber, 1));
+      const hqClasses = reduceByDiv(c.classifications, (r) => r.class);
+      const hqCurrents = reduceByDiv(c.classifications, (r) => Number(r.current_percent));
+      const recalcByCurPercent = calculateUSPSAClassification(memberScores, "curPercent");
+      const recalcByRecPercent = calculateUSPSAClassification(memberScores, "recPercent");
 
       process.stdout.write(".");
-      return mapDivisionsAsync((division) =>
-        Shooter.create({
-          ...shooterDoc,
-          ...classificationsBreakdownAdapter(shooterDoc, division),
-          age: shooterDoc.ages[division],
-          age1: shooterDoc.age1s[division],
+      return mapDivisionsAsync((division) => {
+        const recalcDivCur = reclassificationBreakdown(recalcByCurPercent, division);
+        const recalcDivRec = reclassificationBreakdown(recalcByRecPercent, division);
+
+        const reclassificationsCurPercentCurrent = Number((recalcDivCur?.current ?? 0).toFixed(4));
+        const reclassificationsRecPercentCurrent = Number((recalcDivRec?.current ?? 0).toFixed(4));
+
+        const hqClass = hqClasses[division];
+        const hqCurrent = hqCurrents[division];
+        const hqToCurHHFPercent = hqCurrent - reclassificationsCurPercentCurrent;
+        const hqToRecPercent = hqCurrent - reclassificationsRecPercentCurrent;
+
+        //return Shooter.findOneAndUpdate(
+        //  { memberNumber, division },
+        // { upsert: true }
+        return Shooter.create({
+          data: c.member_data,
+          memberId: c.member_data.member_id,
+          memberNumber,
+          name: [c.member_data.first_name, c.member_data.last_name, c.member_data.suffix]
+            .filter(Boolean)
+            .join(" "),
+
+          hqClass,
+          hqClassRank: rankForClass(hqClass),
+          class: hqClass,
+          classes: hqClasses,
+          currents: hqCurrents,
+          current: hqCurrent,
+
+          // TODO: make ages part of the reclassification
+          ages,
+          age: ages[division],
+          age1s,
+          age1: age1s[division],
+
+          reclassificationsCurPercentCurrent, // aka curHHFPercent
+          reclassificationsRecPercentCurrent, // aka recPercent
+          reclassifications: {
+            curPercent: recalcDivCur,
+            recPercent: recalcDivRec,
+          },
+
+          recClass: recalcDivRec.class,
+          recClassRank: rankForClass(recalcDivRec.class),
+          curHHFClass: recalcDivCur.class,
+          curHHFClassRank: rankForClass(recalcDivCur.class),
+
+          hqToCurHHFPercent,
+          hqToRecPercent,
           division,
-          memberNumberDivision: [shooterDoc.memberNumber, division].join(":"),
-        })
-      );
+          memberNumberDivision: [memberNumber, division].join(":"),
+        });
+      });
     }
   );
   console.timeEnd("shooters");
-};
-
-const reclassificationBreakdown = (reclassificationInfo, division) => {
-  const high = reclassificationInfo?.[division]?.highPercent;
-  const current = reclassificationInfo?.[division]?.percent;
-  return {
-    class: classForPercent(current),
-    classes: mapDivisions((div) =>
-      classForPercent(reclassificationInfo?.[div]?.percent)
-    ),
-    highClass: classForPercent(high),
-    highClasses: mapDivisions((div) =>
-      classForPercent(reclassificationInfo?.[div]?.highPercent)
-    ),
-    high,
-    current: reclassificationInfo?.[division]?.percent,
-    highs: mapDivisions((div) => reclassificationInfo?.[div]?.highPercent),
-    currents: mapDivisions((div) => reclassificationInfo?.[div]?.percent),
-  };
-};
-
-const classificationsBreakdownAdapter = (c, division) => {
-  const {
-    classifications,
-    reclassificationsByCurPercent,
-    reclassificationsByRecPercent,
-    high,
-    current,
-    currents,
-    ...etc
-  } = c;
-  try {
-    const reclassificationsCurPercent = reclassificationBreakdown(
-      reclassificationsByCurPercent,
-      division
-    );
-    const reclassificationsRecPercent = reclassificationBreakdown(
-      reclassificationsByRecPercent,
-      division
-    );
-
-    const reclassificationsCurPercentCurrent = Number(
-      (reclassificationsCurPercent?.current ?? 0).toFixed(4)
-    );
-    const reclassificationsRecPercentCurrent = Number(
-      (reclassificationsRecPercent?.current ?? 0).toFixed(4)
-    );
-
-    const hqClass = classifications?.[division];
-    const hqCurrent = current?.[division];
-    const hqToCurHHFPercent = hqCurrent - reclassificationsCurPercentCurrent;
-    const hqToRecPercent = hqCurrent - reclassificationsRecPercentCurrent;
-
-    return {
-      ...etc,
-      class: classifications?.[division],
-      classes: classifications,
-      high: high?.[division],
-      current: hqCurrent,
-      highs: high,
-      currents: current,
-      // needs to be not-deep for sort
-      reclassificationsCurPercentCurrent,
-      reclassificationsRecPercentCurrent,
-      reclassificationChange:
-        reclassificationsRecPercentCurrent - reclassificationsCurPercentCurrent,
-      reclassifications: {
-        curPercent: reclassificationsCurPercent,
-        recPercent: reclassificationsRecPercent,
-      },
-
-      recClass: reclassificationsRecPercent.class,
-      recClassRank: rankForClass(reclassificationsRecPercent.class),
-      hqClass,
-      hqClassRank: rankForClass(hqClass),
-      curHHFClass: reclassificationsCurPercent.class,
-      curHHFClassRank: rankForClass(reclassificationsCurPercent.class),
-
-      hqToCurHHFPercent,
-      hqToRecPercent,
-      division,
-    };
-  } catch (err) {
-    console.log(err);
-    console.log(division);
-  }
-
-  return {
-    ...c,
-    error: true,
-    class: "X",
-    high: 0,
-    current: 0,
-    division,
-    name: "Expired / Not Found",
-  };
 };
 
 const expiredShootersAggregate = [
