9dd78d5c76e387e2625f213a879b198edff0f432
Howler Monkey <howlermonkey@uspsa.org>
2024-05-22 15:00:54 -0600

    db: add connection logging, reconnect

--- api/src/db/index.js
+++ api/src/db/index.js
@@ -8,7 +8,38 @@ import { hydrateClassifiersExtendedMeta } from "./classifiers.js";
 export const connect = async () => {
   const { QUICK_DEV, MONGO_URL, MONGO_URL_QUICK } = process.env;
   const url = !QUICK_DEV ? MONGO_URL : MONGO_URL_QUICK;
-  await mongoose.connect(url);
+  const _connect = () => {
+    console.error("DB: connecting");
+    return mongoose.connect(url);
+  };
+
+  mongoose.connection.on("error", function (e) {
+    console.error("Mongo connection error:");
+    if (e) {
+      console.error(e);
+    }
+  });
+
+  mongoose.connection.on("disconnected", async () => {
+    console.error("DB: lost connection");
+    await _connect();
+  });
+  mongoose.connection.on("connected", function () {
+    console.error("DB: connected");
+  });
+
+  mongoose.connection.on("reconnected", function () {
+    console.error("DB: reconnected");
+  });
+
+  // Close the Mongoose connection, when receiving SIGINT
+  process.on("SIGINT", function () {
+    mongoose.connection.close(function () {
+      console.error("DB: closed on SIGINT");
+      process.exit(0);
+    });
+  });
+  await _connect();
 };
 
 export const hydrate = async () => {
