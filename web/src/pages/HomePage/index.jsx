24c7a0ea7c4ad0efff54fa5b0db9d0886184dfa6
Howler Monkey <howlermonkey@uspsa.org>
2024-06-23 02:37:47 -0600

    feat(HomePage): add Footer

--- web/src/pages/HomePage/index.jsx
+++ web/src/pages/HomePage/index.jsx
@@ -525,6 +525,30 @@ const HomePage = () => {
           />
         </div>
       </div>
+      <Divider />
+      <div>
+        <div class="surface-section px-4 py-4 md:px-6 lg:px-8 text-center">
+          <img src="/img/home/howlerData.png" alt="Howler Monkey Data, Inc" height="64" />
+          <div class="font-medium text-900 mt-2 mb-1">© 2024 Howler Monkey Data, Inc</div>
+          <p class="text-600 line-height-3 mt-0 mb-4">Made with 🧩 and 🍺 in 🇰🇷</p>
+          <div class="flex align-items-center justify-content-center">
+            <a
+              class="cursor-pointer text-700 mr-5"
+              href="https://instagram.com/codehowlermonkey"
+              target="_blank"
+            >
+              <i class="pi pi-instagram"></i>
+            </a>
+            <a
+              class="cursor-pointer text-700"
+              href="https://github.com/CodeHowlerMonkey/hitfactor.info"
+              target="_blank"
+            >
+              <i class="pi pi-github"></i>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
