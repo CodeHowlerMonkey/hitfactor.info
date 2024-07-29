0fd0b6d9fd811e5c676e13ebb919b0ae044baf84
Howler Monkey <howlermonkey@uspsa.org>
2024-07-29 16:10:32 -0600

    feat(RQ): start adding react-query

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/soho-dark/theme.css";
 import "primeicons/primeicons.css";
@@ -8,10 +9,14 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <PrimeReactProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider queryClient={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </PrimeReactProvider>
   </React.StrictMode>
 );
