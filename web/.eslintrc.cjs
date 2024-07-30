3d849e0aeea109ced18d0c2b592e4bd42af7053c
Howler Monkey <howlermonkey@uspsa.org>
2024-07-29 19:49:06 -0600

    chore(eslint): improve eslint + prettier config

---
+++ web/.eslintrc.cjs
@@ -0,0 +1,53 @@
+const a11yOff = Object.keys(require("eslint-plugin-jsx-a11y").rules).reduce(
+  (acc, rule) => {
+    acc[`jsx-a11y/${rule}`] = "off";
+    return acc;
+  },
+  {}
+);
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "parent", "sibling", "index"],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+    "import/prefer-default-export": 0,
+
+    "react/jsx-props-no-spreading": 0,
+    "react/prop-types": 0,
+    "react/display-name": 0,
+    "react/no-unstable-nested-components": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/function-component-definition": [
+      1,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "react/no-unescaped-entities": 0,
+
+    // TODO: revert and fix these later
+    "react-refresh/only-export-components": 0,
+    ...a11yOff,
+  },
+};
