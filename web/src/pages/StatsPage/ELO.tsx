31875e78d5feddcab861ac2eead4bf72a63b889d
Howler Monkey <howlermonkey@uspsa.org>
2024-12-30 19:14:22 -0700

    feat(rehydrateForCC): rename script, add all ELO except l10

--- web/src/pages/StatsPage/ELO.tsx
+++ web/src/pages/StatsPage/ELO.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+
 import { DivisionNavigation } from "../../components";
 import ShootersELODistributionChart from "../../components/chart/ShootersELODistributionChart";
 
-const ELO = () => (
-  <>
-    <DivisionNavigation uspsaOnly forcedDivision="co" />
-    <ShootersELODistributionChart division="co" />
-  </>
-);
+const ELO = () => {
+  // TODO: react-router params for all of StatsPage
+  const [division, setDivision] = useState();
 
+  return (
+    <>
+      <DivisionNavigation uspsaOnly onSelect={setDivision} />
+      {division && <ShootersELODistributionChart division={division} />}
+    </>
+  );
+};
 export default ELO;
