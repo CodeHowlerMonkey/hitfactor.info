240696220874d85f3be9f3a9cce4e594029c6935
Howler Monkey <howlermonkey@uspsa.org>
2025-01-19 19:36:25 -0700

    fix(log10Targets): use 0.94 multiplier for Kirt numbers

--- shared/utils/log10Targets.ts
+++ shared/utils/log10Targets.ts
@@ -16,10 +16,10 @@ export const coTargetsHFI: Target[] = [
  * Weibulls of CO classifiers to curHHF.
  */
 export const coTargetsFromKirt: Target[] = [
-  [0.0137, 0.95],
-  [0.03, 0.9],
-  [0.0589, 0.85],
-  [0.1697, 0.75],
+  [0.0137 * 0.94, 0.95],
+  [0.03 * 0.94, 0.9],
+  [0.0589 * 0.94, 0.85],
+  [0.1697 * 0.94, 0.75],
 ];
 
 export const log10TargetsHHF = (
